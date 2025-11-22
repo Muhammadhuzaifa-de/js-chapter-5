529804a2cdc75384b331d1e9431904a82f633599
Muhammadhuzaifa-de <huzzi130@gmai.com>
2025-11-22 18:52:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,115 @@
+// adition 
+console.log("sum of 3+5 is",
+    3+5
+)
+//subtraction
+
+console.log("subtraction of 5-3 is",
+    5-3
+)
+
+// modolus
+
+console.log("modolus of 10%3 is",
+    10 % 3 
+
+)
+
+
+var totalMarks = 100 ;
+
+console.log ( 
+ "subtraction of totalMarks is  " ,   totalMarks - 50
+)
+
+
+
+var gainMarks = 70 ;
+
+console.log ( 
+ "addition of totalMarks is  " ,   gainMarks + 20
+
+)
+
+
+var percentage = 100 % 80 ;
+
+console.log ( 
+ "Percentage is " , percentage
+)
+
+
+
+var value = 5 ;
+
+console.log ("initial value is" ,
+    value
+)
+
+
+
+var value = 6 ;
+
+console.log ( "value after increment is" ,
+    value
+)
+
+
+var value = 13 ;
+
+console.log ( "value after addition is" ,
+    value
+)
+
+
+var value = 12 ;
+
+console.log ( "value after decrement is" ,
+    value
+)
+
+
+var value = 0 ;
+
+console.log ( "the reminder is" ,
+    value
+)
+
+
+// var tableOf = +prompt("Enter a number to print it's table:");
+
+// for (var i = 1; i <= 10; i++) {
+//   console.log(tableOf + " x " + i + " = ", tableOf *i);
+// }
+
+var  priceOf1items = 650;
+
+var quantity1 =  3 ;
+
+var  priceOf2items = 100;
+
+var quantity2=  7;
+
+var shipping = 100;
+
+
+var totalCost = ( priceOf1items * 3) +  (priceOf2items * 7 ) + 100 ;
+ 
+
+
+    totalCost
+console.log("totalcost ",
+    totalCost
+)
+
+
+// marks sheet
+
+var totalNum = 1150;
+var myNum = 849;
+
+var percentage = (myNum / totalNum) * 100;
+
+console.log(percentage.toFixed(2) ,"Percantange"); // outputs: 73.83
+
+
